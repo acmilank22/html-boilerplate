f5952f425fe086b68352fbf1f7e31203b4ec2111
Dongyi An <acmilank22@gmail.com>
2018-05-10 10:07:36 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
-async function request(url) {
-    try {
-        const request = await fetch(url, { mode: 'no-cors' });
-        console.log(request);
-        return request.text();
-    } catch (error) {
-        throw new Error(error);
-    }
-}
-
-(async () => {
-    const data = await request('http://127.0.0.1:8080/');
-    console.log(data);
-})();
+document.getElementById('testButton').addEventListener(
+    'touchstart',
+    function() {
+        alert('touchstart');
+    },
+    false
+);
