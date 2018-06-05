9c6ddb0471f840cf92356095254811fa46a2b0db
Dongyi An <acmilank22@gmail.com>
2018-06-05 11:01:42 +0800

    update

--- js/app.js
+++
@@ -1,6 +0,0 @@
-fetch('http://139.199.112.161/qfappmall_order/order_trade.do', {
-    method: 'POST'
-})
-    .then(data => data.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
