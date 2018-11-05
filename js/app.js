fedd8f87473a25fcdbf74bb4afb8dcf9cfdff025
Dongyi An <acmilank22@gmail.com>
2018-11-05 15:21:05 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,35 +1,5 @@
-const o = {
-  a: 1,
-  b: {
-    c: [
-      2,
-      {
-        d: 3
-      }
-    ],
-    e: {
-      f: {
-        g: 4
-      }
-    }
-  },
-  h: [
-    {
-      i: 5
-    },
-    {
-      j: { k: 6 }
-    }
-  ]
-};
-
-// const s = qs.stringify(o);
-
-// console.log(s);
-// console.log(qs.parse(s));
-
-define(['js/libs/qs@latest.js', 'js/libs/jquery.min'], function(qs, $) {
+define(['js/libs/qs@latest.js'], function(qs) {
   return {
-    s: qs.stringify(o)
+    stringify: qs.stringify
   };
 });
--- js/index.js
+++ js/index.js
@@ -3,6 +3,29 @@ requirejs.config({
 });
 
 requirejs(['app'], function(app) {
-  console.log(app);
-  app.s;
+  const o = {
+    a: 1,
+    b: {
+      c: [
+        2,
+        {
+          d: 3
+        }
+      ],
+      e: {
+        f: {
+          g: 4
+        }
+      }
+    },
+    h: [
+      {
+        i: 5
+      },
+      {
+        j: { k: 6 }
+      }
+    ]
+  };
+  console.log(app.stringify(o));
 });
