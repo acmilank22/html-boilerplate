66cf82f3176dd4a4b4e23210b9a2ae17fbd169bb
Dongyi An <acmilank22@gmail.com>
2020-05-16 22:28:52 +0800

    update

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function debounce(func, delay) {
+  let id = null;
+  return function () {
+    clearTimeout(id);
+    id = setTimeout(() => {
+      func.apply(this, arguments);
+    }, delay);
+  };
+}
+
+function throttle(func, delay) {
+  let isExec = true;
+  return function () {
+    if (!isExec) {
+      return;
+    }
+    isExec = false;
+    setTimeout(() => {
+      func.apply(this, arguments);
+      isExec = true;
+    }, delay);
+  };
+}
+
+console.log(debounce, throttle);
+
+const fn = (a) => {
+  return a;
+};
+
+fn();
