3083e2da1adabd29222e748f05226aa8b026917d
Dongyi An <acmilank22@gmail.com>
2020-06-30 00:18:47 +0800

    ci: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:node/recommended',
+    'plugin:prettier/recommended',
+  ],
   env: {
     browser: true,
     'shared-node-browser': true,
@@ -6,44 +11,28 @@ module.exports = {
     node: true,
     commonjs: true,
     es6: true,
-    worker: true,
+    es2017: true,
+    es2020: true,
   },
-  plugins: ['html', 'import', 'node'],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['html'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
   rules: {
-    curly: 'warn',
-    'default-case': 'warn',
-    'no-extra-bind': 'warn',
-    'no-extra-label': 'warn',
-    'no-useless-call': 'warn',
-    'init-declarations': 'error',
-    'no-label-var': 'error',
-    'no-use-before-define': ['error', { functions: false }],
-    indent: ['warn', 2, { SwitchCase: 1 }],
-    semi: 'error',
     'node/no-unsupported-features/es-builtins': [
       'error',
-      { version: '>=10.0.0', ignores: [] },
+      { version: '>=12.0.0', ignores: [] },
     ],
     'node/no-unsupported-features/es-syntax': [
       'error',
-      { version: '>=10.0.0', ignores: ['modules'] },
+      { version: '>=12.0.0', ignores: ['modules'] },
     ],
     'node/no-unsupported-features/node-builtins': [
       'error',
-      { version: '>=10.0.0', ignores: [] },
+      { version: '>=12.0.0', ignores: [] },
     ],
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    parser: 'babel-eslint',
   },
   overrides: [
     {
